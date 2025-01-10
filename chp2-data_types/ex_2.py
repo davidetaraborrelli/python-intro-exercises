4b3e4216e7b3390e01c41593b759f5b14d038cb8
Davide Taraborrelli <taraborrellidavide@gmail.com>
2025-01-10 18:20:50 +0100

    completed chapter exercise

---
+++ chp2-data_types/ex_2.py
@@ -0,0 +1,23 @@
+# Create a tuple called names 
+# that contains several pet names.
+
+names = ('Asta',
+         'Butterscotch',
+         'Pudding',
+         'Neptune',
+         'Darwin',
+) # Tuple of names
+
+
+# Create a dictionary named pets that
+# contains a list of pet names and the type of animal.
+
+pets = {'Asta': 'dog',
+        'Butterscotch': 'cat',
+        'Pudding': 'cat',
+        'Neptune': 'fish',
+        'Darwin': 'lizard',
+} # Dictionary of pets
+
+print(names)
+print(pets)
