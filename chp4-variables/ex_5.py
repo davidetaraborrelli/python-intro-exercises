2de830496550df0ad15f976f434899ae177ad174
Davide Taraborrelli <taraborrellidavide@gmail.com>
2025-01-13 20:19:00 +0100

    finished chapter 4 exercises

---
+++ chp4-variables/ex_5.py
@@ -0,0 +1,12 @@
+# Using concatenation to print a greeting message
+name = 'Victor'
+print('Good Morning, ' + name + '.')
+print('Good Afternoon, ' + name + '.')
+print('Good Evening, ' + name + '.')
+
+# Using string interpolation to print a greeting message
+
+name = 'Victor'
+print(f'Good Morning, {name}.')
+print(f'Good Afternoon, {name}.')
+print(f'Good Evening, {name}.')
