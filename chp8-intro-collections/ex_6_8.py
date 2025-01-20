bfe9a96a3074a943a1b3d6d1324ebf5ac4fab791
Davide Taraborrelli <taraborrellidavide@gmail.com>
2025-01-20 20:43:44 +0100

    done part of chapter 8 exercises

---
+++ chp8-intro-collections/ex_6_8.py
@@ -0,0 +1,9 @@
+# Exercise 6
+# Write some code that converts the value of pi to a string and assigns it to a variable named str_pi.
+
+pi = 3.141592
+str_pi = str(pi)
+
+# Exercise 8
+range(3, 17, 4)
+print(list(range(3, 17, 4)))
